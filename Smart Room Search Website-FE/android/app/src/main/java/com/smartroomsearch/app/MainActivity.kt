2d57a0d7ee8ed35f5f1902567286e5faa05a8de7
vinh0407 <thu.dvc75@gmail.com>
2026-08-19 17:24:36 +0700

    fix: tidb() thay {param} bang gia tri that trong URL path (fix view/contact, chuan hoa DS calls)

--- Smart Room Search Website-FE/android/app/src/main/java/com/smartroomsearch/app/MainActivity.kt
+++ Smart Room Search Website-FE/android/app/src/main/java/com/smartroomsearch/app/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.smartroomsearch.app
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.getcapacitor.BridgeActivity
 
-class MainActivity : BridgeActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
